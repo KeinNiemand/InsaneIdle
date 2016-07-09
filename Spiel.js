50e950b840516763d87215fc21562276b42fab9b
KeinNiemand <Emty@aaaa.de>
2016-07-09 13:19:10 +0200

    Bug Fixes

--- Spiel.js
+++ Spiel.js
@@ -157,6 +157,7 @@ function AKaufen() {
 		MaxAKauf ()
 	}
 	else {
+		MKaufPrRch()
 		AKauf()
 	}
 }
@@ -179,7 +180,6 @@ function BKauf () {
 		//}
 	//} 
 	//else {
-		
 		if (Sp.Geld.gte(Sp.BPreis)) {
 			Sp.Geld = Sp.Geld.sub(Sp.BPreis)
 			Sp.BPreis = Sp.BPreis.mul(PreisErhB)
